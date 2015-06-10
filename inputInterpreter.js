6b262f3e3f3ac5cb5b251662d7e43fb855f7f931
Julian Haeger <julianhaeger@gmail.com>
2015-06-10 22:47:07 +0100

    can create flowers whereever by tapping

--- inputInterpreter.js
+++ inputInterpreter.js
@@ -1,5 +1,11 @@
-userInputEvents.on('game area tap', function () {
-	var gameLocation = vp.getCentre();
+userInputEvents.on('game area tap', function (ev) {
+
+	var gameLocation = {
+		x: vp.getCentre().x,
+		y: vp.getCentre().y
+	};
+	gameLocation.x += (ev.clientX - vp.size.width / 2);
+	gameLocation.y += (ev.clientY - vp.size.height / 2);
 	console.log('place game item at', gameLocation)
 	userCommands.emit('place game item', {
 		location: gameLocation,
